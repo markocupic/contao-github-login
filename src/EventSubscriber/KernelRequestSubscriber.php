29cd1c2b2ada53548b9e4a25ad6ceb264710b982
Marko Cupic <m.cupic@gmx.ch>
2026-03-15 21:45:02 +0100

    Update the tools

--- src/EventSubscriber/KernelRequestSubscriber.php
+++ src/EventSubscriber/KernelRequestSubscriber.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of Contao GitHub Login.
  *
- * (c) Marko Cupic 2024 <m.cupic@gmx.ch>
+ * (c) Marko Cupic 2026 <m.cupic@gmx.ch>
  * @license GPL-3.0-or-later
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
@@ -23,7 +23,7 @@ use Symfony\Component\HttpKernel\KernelEvents;
 readonly class KernelRequestSubscriber implements EventSubscriberInterface
 {
     public function __construct(
-        private readonly Packages $packages,
+        private Packages $packages,
         private ScopeMatcher $scopeMatcher,
     ) {
     }
